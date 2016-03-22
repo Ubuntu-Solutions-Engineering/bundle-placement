6a3f6ac24ce216262a42eface0de82c408de27a5
Michael McCracken <mike.mccracken@canonical.com>
2016-03-22 18:30:20 +0000

    add __main__ to make pyflakes happy
    allows us to run bundleplacer as module with python -m
    
    Signed-off-by: Michael McCracken <mike.mccracken@canonical.com>

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     entry_points={
         "console_scripts": [
-            "bundle-placer = bundleplacer.cli:main"
+            "bundle-placer = bundleplacer.__main__:main"
         ]
     }
 )
